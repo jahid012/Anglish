8836305921ea4bc3b8476b70588503b3ba339370
Jahid <jahidhasanbadhon1999@gmail.com>
2022-04-12 15:03:37 +0600

    Firebase ImageUpload

--- app/src/main/java/com/learn/anglish/MainActivity.java
+++ app/src/main/java/com/learn/anglish/MainActivity.java
@@ -1,51 +1,129 @@
 package com.learn.anglish;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ImageView img;
+    Button browse, upload;
+    Uri filepath;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = (ImageView) findViewById(R.id.imageView);
+        browse= (Button) findViewById(R.id.btn1);
+        upload = (Button) findViewById(R.id.btn2);
+
+        browse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dexter.withContext(MainActivity.this)
+                        .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        .withListener(new PermissionListener() {
+                            @Override
+                            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                                Intent intent = new Intent(Intent.ACTION_PICK);
+                                intent.setType("image/*");
+                                startActivityForResult(Intent.createChooser(intent,"please Select An Image"), 111);
+
+                            }
+
+                            @Override
+                            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                                permissionToken.continuePermissionRequest();
+                            }
+                        }).check();
+            }
+        });
+
+        //Upload
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadToFirebase();
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 111 && resultCode == RESULT_OK){
+            filepath = data.getData();
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(filepath);
+                bitmap = BitmapFactory.decodeStream(inputStream);
+                img.setImageBitmap(bitmap);
+            }catch (Exception Ex){
+
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
-    public void process(View view) {
+    private void uploadToFirebase() {
+        final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+        dialog.setTitle("File Uploaded");
+        dialog.show();
 
-        EditText t0 = (EditText)findViewById(R.id.t0);
-        EditText t1 = (EditText)findViewById(R.id.t1);
-        EditText t2 = (EditText)findViewById(R.id.t2);
-        EditText t3 = (EditText)findViewById(R.id.t3);
-
-        String roll = t0.getText().toString().trim();
-        String name = t1.getText().toString().trim();
-        String course = t2.getText().toString().trim();
-        String duration = t3.getText().toString().trim();
-
-
-
-        DataHolder data = new DataHolder(name, course, duration);
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference root = database.getReference("Student");
-
-        root.child(roll).setValue(data);
-
-        t0.setText("");
-        t1.setText("");
-        t2.setText("");
-        t3.setText("");
-
-        Toast.makeText(this, "Data Inserted Successfully.", Toast.LENGTH_SHORT).show();
-        
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference root = storage.getReference().child("image1");
+        root.putFile(filepath)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        dialog.dismiss();
+                        Toast.makeText(MainActivity.this, "Successfully Uploaded", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        float persent = (100 * snapshot.getBytesTransferred() /snapshot.getTotalByteCount());
+                        dialog.setMessage("Uploaded" + (int)persent+"%");
+                    }
+                });
     }
 }
