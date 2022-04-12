0bad8bef9ce719d4dde498b82b86dbedd39e75b6
Jahid <jahidhasanbadhon1999@gmail.com>
2022-04-12 11:59:53 +0600

    Multiple Data Upload

---
+++ app/src/main/java/com/learn/anglish/DataHolder.java
@@ -0,0 +1,35 @@
+package com.learn.anglish;
+
+public class DataHolder {
+    String name, course, duration;
+
+    public DataHolder(String name, String course, String duration) {
+        this.name = name;
+        this.course = course;
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+}
--- app/src/main/java/com/learn/anglish/MainActivity.java
+++ app/src/main/java/com/learn/anglish/MainActivity.java
@@ -21,12 +21,31 @@ public class MainActivity extends AppCompatActivity {
 
     public void process(View view) {
 
-        EditText t1 = (EditText)findViewById(R.id.editText);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference root = database.getReference();
+        EditText t0 = (EditText)findViewById(R.id.t0);
+        EditText t1 = (EditText)findViewById(R.id.t1);
+        EditText t2 = (EditText)findViewById(R.id.t2);
+        EditText t3 = (EditText)findViewById(R.id.t3);
 
-        root.setValue(t1.getText().toString());
+        String roll = t0.getText().toString().trim();
+        String name = t1.getText().toString().trim();
+        String course = t2.getText().toString().trim();
+        String duration = t3.getText().toString().trim();
+
+
+
+        DataHolder data = new DataHolder(name, course, duration);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference root = database.getReference("Student");
+
+        root.child(roll).setValue(data);
+
+        t0.setText("");
         t1.setText("");
-        Toast.makeText(this, "Data Inseted", Toast.LENGTH_SHORT).show();
+        t2.setText("");
+        t3.setText("");
+
+        Toast.makeText(this, "Data Inserted Successfully.", Toast.LENGTH_SHORT).show();
+        
     }
 }
